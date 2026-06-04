7813ef71880b103d4d3b03cb30edad7972f27fab
afaq imanova <afaq@imanov.name>
2026-06-04 08:50:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace SortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.SortedList auzef = new System.Collections.SortedList();
+            auzef.Add("BLPR", "Bilgisayar Programciligi");
+            auzef.Add("IKAO", "Iktisat");
+            auzef.Add("ISAO", "Isletme");
+            auzef.Add("ILHA", "Ilahiyat");
+            //Elemanlar listeleniyor
+            foreach(string eleman in auzef.Keys)
+                Console.WriteLine(eleman + ": "+ auzef[eleman]);
+        }
+    }
+}
